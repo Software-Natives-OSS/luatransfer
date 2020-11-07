0be55f5c489c83b7be91838533a70bf3f6867264
Raphael Zulliger <zulliger@software-natives.ch>
2020-11-07 11:25:15 +0100

    Implemented more wrappers along with tests

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const ffi = require("ffi-napi");
+const ref = require("ref-napi")
+
+const stringBuffer = 'string'
+const luaChannelDescriptor = ref.types.size_t;
+const luaChannelDescriptorPtr = ref.refType(luaChannelDescriptor);
+// typedef void (*TransferLua_WriterFunction) (const char* p, void* ud);
+const writerPtr = 'pointer';
+
 
 const transferlua = ffi.Library('libtransferlua', {
-
+    // TRANSFERLUA_EXPORT  uint64 WINAPI TransferLuaOpen		(const char* TargetPath, const char* Endpoint, tTransferLuaChannelDescriptor* ChannelDescriptor, TransferLua_Writer* ErrorWriter);
+    'TransferLuaOpen': ['uint64', [stringBuffer, stringBuffer, luaChannelDescriptorPtr, writerPtr]],
+    // TRANSFERLUA_EXPORT  uint64 WINAPI TransferLuaForceOpen		(const char* TargetPath, const char* Endpoint, tTransferLuaChannelDescriptor* ChannelDescriptor, TransferLua_Writer* ErrorWriter);
+    'TransferLuaForceOpen': ['uint64', [stringBuffer, stringBuffer, luaChannelDescriptorPtr, writerPtr]],
+    // TRANSFERLUA_EXPORT  void WINAPI TransferLuaClose		(tTransferLuaChannelDescriptor ChannelDescriptor);
+    'TransferLuaClose': ['uint64', [luaChannelDescriptor]],
+    // TRANSFERLUA_EXPORT  uint64 WINAPI TransferLuaSendChunk	 (
+    //    tTransferLuaChannelDescriptor ChannelDescriptor, const char* ScriptName, const char* StateName, const char* TaskName, 
+    //    const char* DataOrModuleName, const char* Chunk, uint32 ChunkSize, const char* Options, TransferLua_Writer* ErrorWriter);
+    'TransferLuaSendChunk': ['uint64', [luaChannelDescriptor, 'string', 'string', 'string', 'string', 'string', 'uint32', 'string', writerPtr]],
+    // TRANSFERLUA_EXPORT  uint64 WINAPI TransferLuaSendFile	 (tTransferLuaChannelDescriptor ChannelDescriptor, const char* FileName, 
+    //    const char* StateName, const char* TaskName, const char* DataOrModuleName, const char* Options, TransferLua_Writer* ErrorWriter);
+    'TransferLuaSendFile': ['uint64', [luaChannelDescriptor, 'string', 'string', 'string', 'string', 'string', writerPtr]]
 });
 
-module.exports = transferlua
+module.exports = {
+    open: transferlua.TransferLuaOpen,
+    forceOpen: transferlua.TransferLuaForceOpen,
+    close: transferlua.TransferLuaClose,
+    sendChunk: transferlua.TransferLuaSendChunk,
+    sendFile: transferlua.TransferLuaSendFile
+}
--- test/loadlibrary.test.js
+++ test/loadlibrary.test.js
@@ -1,16 +1,44 @@
 const assert = require('assert');
+const ref = require('ref-napi')
 
-const indel_test_target = 'TransferLuaTest';
+const indelTestTarget = 'TransferLuaTest';
+const luaEndpoint = 'Default';
 
 function loadWrapper() {
     return require("../index");
 }
 describe('require module', () => {
+
+    beforeEach(() => {
+        this.outHandle = ref.alloc('size_t');
+    });
+
+    afterEach(() => {
+        var handle = this.outHandle.deref();
+        if (handle > 0) {
+            loadWrapper().close(handle)
+        }
+    });
+
     it('Should find the dynamic library', () => {
         loadWrapper()
        });
 
-    // it('Can open a transfer to the test target', () => {
-    //     loadWrapper.open()
-    // });
+       it('Can open a transfer to the test target', () => {
+        var err = loadWrapper().open(indelTestTarget, luaEndpoint, this.outHandle, null)
+        assert.strictEqual(err, 0, "Expected success");
+    });
+
+    it('Can force open a transfer to the test target', () => {
+        var err = loadWrapper().forceOpen(indelTestTarget, luaEndpoint, this.outHandle, null)
+        assert.strictEqual(err, 0, "Expected success");
+    });
+
+    it('Consecutive call to open failes', () => {
+        var err = loadWrapper().open(indelTestTarget, luaEndpoint, this.outHandle, null)
+        assert.strictEqual(err, 0, "Expected success");
+        var err = loadWrapper().open(indelTestTarget, luaEndpoint, this.outHandle, null)
+        assert.strictEqual(err, 0x100080004, "Expected failure ('already opened')");
+    });
+
 });
